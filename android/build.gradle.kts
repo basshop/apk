6d2232423718a71fa7e35cd438e76cb45e122a26
Your Name <755jkm@gmail.com>
2026-01-02 15:53:13 +0700

    force update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,7 +1,7 @@
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
-    }
+plugins {
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
 
 tasks.register<Delete>("clean") {
